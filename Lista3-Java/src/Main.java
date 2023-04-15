7f13e712291f02ad2cab04fc943d5467066f0671
GABRIELA SCHIRMER <gabriela_schirmer@estudante.sesisenai.org.br>
2023-04-14 21:45:38 -0300

    Lista 3 - Java - feito até 6

--- Lista3-Java/src/Main.java
+++ Lista3-Java/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static Scanner leitor = new Scanner(System.in);
     public static void main(String[] args) {
-        exe4();
+        exe7();
     }
 
     public static void exe1(){
@@ -29,8 +29,35 @@ public class Main {
     }
 
     public static void exe4(){
-        System.out.println("Informe um valor: ");
+        System.out.print("Informe um valor: ");
         int n = leitor.nextInt();
+        if(n>0){
+            for(int i=1;i<=n;i++) {
+                System.out.print(i + " ");
+            }
+        } else {
+            System.out.println("Valor inválido!");
+        }
+    }
 
+    public static void exe5(){
+        int n = 0;
+        while (n <= 0) {
+            System.out.print("Informe um valor: ");
+            n = leitor.nextInt();
+        }
+        for(int i=1;i<=n;i++) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void exe6(){
+        for (int num=1;num<=10;num++){
+            int tab = num * 8;
+            System.out.print(tab+" ");
+        }
+    }
+
+    public static void exe7(){
     }
 }
